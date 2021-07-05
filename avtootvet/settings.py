a3179f03c686b1009408f997b10353b3d98dea88
ruselbyru <r.schetko@gmail.com>
2021-07-05 23:36:16 +0300

    modif settings

--- avtootvet/settings.py
+++ avtootvet/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR.joinpath('static/'),
 ]
-
+STATIC_ROOT= BASE_DIR.joinpath('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
